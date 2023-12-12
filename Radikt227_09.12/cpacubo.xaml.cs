db810b921a0959564bff58f12b3cb1cedc73bb52
Radiktolk <151833196+Radiktolk@users.noreply.github.com>
2023-12-12 15:59:18 +0300

    Update cpacubo.xaml.cs

--- Radikt227_09.12/cpacubo.xaml.cs
+++ Radikt227_09.12/cpacubo.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace antonov227_09._12
+namespace Radik227_09._12
 {
     /// <summary>
     /// Логика взаимодействия для cpacubo.xaml
