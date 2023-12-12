164b07956a5eb99038ea0d54816e933e8a03c59a
Radiktolk <151833196+Radiktolk@users.noreply.github.com>
2023-12-12 15:59:59 +0300

    Update kontakt.xaml.cs

--- Radikt227_09.12/kontakt.xaml.cs
+++ Radikt227_09.12/kontakt.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace antonov227_09._12
+namespace Radik227_09._12
 {
     /// <summary>
     /// Логика взаимодействия для kontakt.xaml
